035852317a370e6421250defb1725fc87028c77c
Z0ro0 <chlrkdmf0805@gmail.com>
2023-05-13 19:28:24 +0900

    네브바, 배경색, 포스트잇 여백 주기

--- src/App.js
+++ src/App.js
@@ -2,27 +2,33 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-    <img class="post" src="./img/post1.png"/>
-    <img class="post" src="./img/post2.png"/>
-    <img class="post" src="./img/post3.png"/>
-    <img class="post" src="./img/post4.png"/>
-    <img class="post" src="./img/post5.png"/>
-    <img class="post" src="./img/post6.png"/>
-    <img class="post" src="./img/post1.png"/>
-    <img class="post" src="./img/post2.png"/>
-    <img class="post" src="./img/post3.png"/>
-    <img class="post" src="./img/post4.png"/>
-    <img class="post" src="./img/post5.png"/>
-    <img class="post" src="./img/post6.png"/>
-    <img class="post" src="./img/post1.png"/>
-    <img class="post" src="./img/post2.png"/>
-    <img class="post" src="./img/post3.png"/>
-    <img class="post" src="./img/post4.png"/>
-    <img class="post" src="./img/post5.png"/>
-    <img class="post" src="./img/post6.png"/>
+    <div id="container">
+      <div id="nav">
+        <img src="./img/circle.png" id="circle"/>
+      </div>
+      <div id="post-container">
+          <img class="post" src="./img/post1.png"/>
+          <img class="post" src="./img/post2.png"/>
+          <img class="post" src="./img/post3.png"/>
+          <img class="post" src="./img/post4.png"/>
+          <img class="post" src="./img/post5.png"/>
+          <img class="post" src="./img/post6.png"/>
+          <img class="post" src="./img/post1.png"/>
+          <img class="post" src="./img/post2.png"/>
+          <img class="post" src="./img/post3.png"/>
+          <img class="post" src="./img/post4.png"/>
+          <img class="post" src="./img/post5.png"/>
+          <img class="post" src="./img/post6.png"/>
+          <img class="post" src="./img/post1.png"/>
+          <img class="post" src="./img/post2.png"/>
+          <img class="post" src="./img/post3.png"/>
+          <img class="post" src="./img/post4.png"/>
+          <img class="post" src="./img/post5.png"/>
+          <img class="post" src="./img/post6.png"/>
+          <img class="post" src="./img/post5.png"/>
+          <img class="post" src="./img/post6.png"/>
+         </div>
     </div>
-    
   );
 }
 
