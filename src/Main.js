482fc4eecd949cd0694fc6de056a44a38c5da32f
Z0ro0 <chlrkdmf0805@gmail.com>
2023-05-14 02:00:19 +0900

    포스트잇 간격 수정, 글쓰기 버튼 고정, p 태그 겹치는 오류 수정 필요

--- src/Main.js
+++ src/Main.js
@@ -10,26 +10,42 @@ function App() {
       </div>
       <div id="post-add-container">
         <div id="post-container">
-            <img class="post" src="./img/post1.png"/>
-            <img class="post" src="./img/post2.png"/>
-            <img class="post" src="./img/post3.png"/>
-            <img class="post" src="./img/post4.png"/>
-            <img class="post" src="./img/post5.png"/>
-            <img class="post" src="./img/post6.png"/>
-            <img class="post" src="./img/post1.png"/>
-            <img class="post" src="./img/post2.png"/>
-            <img class="post" src="./img/post3.png"/>
-            <img class="post" src="./img/post4.png"/>
-            <img class="post" src="./img/post5.png"/>
-            <img class="post" src="./img/post6.png"/>
-            <img class="post" src="./img/post1.png"/>
-            <img class="post" src="./img/post2.png"/>
-            <img class="post" src="./img/post3.png"/>
-            <img class="post" src="./img/post4.png"/>
-            <img class="post" src="./img/post5.png"/>
-            <img class="post" src="./img/post6.png"/>
-            <img class="post" src="./img/post5.png"/>
-            <img class="post" src="./img/post6.png"/>
+          <div class="post">
+            <p class="post-text">응원할게!</p>
+            <img class="post-img" src="./img/post1.png"/>
+          </div>
+          <div class="post">
+            <p class="post-text">응원할게!</p>
+            <img class="post-img" src="./img/post1.png"/>
+          </div>
+          <div class="post">
+            <p class="post-text">응원할게!</p>
+            <img class="post-img" src="./img/post1.png"/>
+          </div>
+          <div class="post">
+            <p class="post-text">응원할게!</p>
+            <img class="post-img" src="./img/post1.png"/>
+          </div>
+            <img class="post-img" src="./img/post1.png"/>
+            <img class="post-img" src="./img/post2.png"/>
+            <img class="post-img" src="./img/post3.png"/>
+            <img class="post-img" src="./img/post4.png"/>
+            <img class="post-img" src="./img/post5.png"/>
+            <img class="post-img" src="./img/post6.png"/>
+            <img class="post-img" src="./img/post1.png"/>
+            <img class="post-img" src="./img/post2.png"/>
+            <img class="post-img" src="./img/post3.png"/>
+            <img class="post-img" src="./img/post4.png"/>
+            <img class="post-img" src="./img/post5.png"/>
+            <img class="post-img" src="./img/post6.png"/>
+            <img class="post-img" src="./img/post1.png"/>
+            <img class="post-img" src="./img/post2.png"/>
+            <img class="post-img" src="./img/post3.png"/>
+            <img class="post-img" src="./img/post4.png"/>
+            <img class="post-img" src="./img/post5.png"/>
+            <img class="post-img" src="./img/post6.png"/>
+            <img class="post-img" src="./img/post5.png"/>
+            <img class="post-img" src="./img/post6.png"/>
           </div>
           <img src="./img/add.png" id="add-button"/>
       </div>
