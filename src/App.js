7f02901b244880df8a4ab527b1e6f9ff1b35a747
Z0ro0 <chlrkdmf0805@gmail.com>
2023-05-14 00:09:22 +0900

    npm i react-router-dom@5.2.0으로 버전 낮추어서 라우터 오류 해결

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
 import './App.css';
+import React from 'react';
+import { Route } from "react-router-dom";
+import Post from './Post.js';
+import Main from './Main.js';
 
 function App() {
   return (
-    <div id="container">
-      <div id="nav">
-        <div></div>
-        <p id="owner-text">000의 익명 메시지</p>
-        <img src="./img/circle.png" id="circle"/>
-      </div>
-      <div id="post-add-container">
-        <div id="post-container">
-            <img class="post" src="./img/post1.png"/>
-            <img class="post" src="./img/post2.png"/>
-            <img class="post" src="./img/post3.png"/>
-            <img class="post" src="./img/post4.png"/>
-            <img class="post" src="./img/post5.png"/>
-            <img class="post" src="./img/post6.png"/>
-            <img class="post" src="./img/post1.png"/>
-            <img class="post" src="./img/post2.png"/>
-            <img class="post" src="./img/post3.png"/>
-            <img class="post" src="./img/post4.png"/>
-            <img class="post" src="./img/post5.png"/>
-            <img class="post" src="./img/post6.png"/>
-            <img class="post" src="./img/post1.png"/>
-            <img class="post" src="./img/post2.png"/>
-            <img class="post" src="./img/post3.png"/>
-            <img class="post" src="./img/post4.png"/>
-            <img class="post" src="./img/post5.png"/>
-            <img class="post" src="./img/post6.png"/>
-            <img class="post" src="./img/post5.png"/>
-            <img class="post" src="./img/post6.png"/>
-          </div>
-          <img src="./img/add.png" id="add-button"/>
-      </div>
+    <div>
+          <Route path="/" component={Main}/>
+          <Route path="/Post" component={Post}/>
     </div>
+    // <div id="container">
+    //   <div id="nav">
+    //     <div></div>
+    //     <p id="owner-text">000의 익명 메시지</p>
+    //     <img src="./img/circle.png" id="circle"/>
+    //   </div>
+    //   <div id="post-add-container">
+    //     <div id="post-container">
+    //         <img class="post" src="./img/post1.png"/>
+    //         <img class="post" src="./img/post2.png"/>
+    //         <img class="post" src="./img/post3.png"/>
+    //         <img class="post" src="./img/post4.png"/>
+    //         <img class="post" src="./img/post5.png"/>
+    //         <img class="post" src="./img/post6.png"/>
+    //         <img class="post" src="./img/post1.png"/>
+    //         <img class="post" src="./img/post2.png"/>
+    //         <img class="post" src="./img/post3.png"/>
+    //         <img class="post" src="./img/post4.png"/>
+    //         <img class="post" src="./img/post5.png"/>
+    //         <img class="post" src="./img/post6.png"/>
+    //         <img class="post" src="./img/post1.png"/>
+    //         <img class="post" src="./img/post2.png"/>
+    //         <img class="post" src="./img/post3.png"/>
+    //         <img class="post" src="./img/post4.png"/>
+    //         <img class="post" src="./img/post5.png"/>
+    //         <img class="post" src="./img/post6.png"/>
+    //         <img class="post" src="./img/post5.png"/>
+    //         <img class="post" src="./img/post6.png"/>
+    //       </div>
+    //       <img src="./img/add.png" id="add-button"/>
+    //   </div>
+    // </div>
   );
 }
 
---
+++ src/Main.js
@@ -0,0 +1,39 @@
+import './App.css';
+
+function App() {
+  return (
+    <div id="container">
+      <div id="nav">
+        <p id="owner-text">000의 익명 메시지</p>
+        <img src="./img/circle.png" id="circle"/>
+      </div>
+      <div id="post-add-container">
+        <div id="post-container">
+            <img class="post" src="./img/post1.png"/>
+            <img class="post" src="./img/post2.png"/>
+            <img class="post" src="./img/post3.png"/>
+            <img class="post" src="./img/post4.png"/>
+            <img class="post" src="./img/post5.png"/>
+            <img class="post" src="./img/post6.png"/>
+            <img class="post" src="./img/post1.png"/>
+            <img class="post" src="./img/post2.png"/>
+            <img class="post" src="./img/post3.png"/>
+            <img class="post" src="./img/post4.png"/>
+            <img class="post" src="./img/post5.png"/>
+            <img class="post" src="./img/post6.png"/>
+            <img class="post" src="./img/post1.png"/>
+            <img class="post" src="./img/post2.png"/>
+            <img class="post" src="./img/post3.png"/>
+            <img class="post" src="./img/post4.png"/>
+            <img class="post" src="./img/post5.png"/>
+            <img class="post" src="./img/post6.png"/>
+            <img class="post" src="./img/post5.png"/>
+            <img class="post" src="./img/post6.png"/>
+          </div>
+          <img src="./img/add.png" id="add-button"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
