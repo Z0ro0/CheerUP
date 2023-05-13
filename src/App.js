b03b453109d0f72786808ea801457ce1caffebde
Z0ro0 <chlrkdmf0805@gmail.com>
2023-05-14 00:15:12 +0900

    라우터 연결, 네브바 css 수정

--- src/App.js
+++ src/App.js
@@ -10,38 +10,6 @@ function App() {
           <Route path="/" exact={true} component={Main}/>
           <Route path="/Post" component={Post}/>
     </div>
-    // <div id="container">
-    //   <div id="nav">
-    //     <div></div>
-    //     <p id="owner-text">000의 익명 메시지</p>
-    //     <img src="./img/circle.png" id="circle"/>
-    //   </div>
-    //   <div id="post-add-container">
-    //     <div id="post-container">
-    //         <img class="post" src="./img/post1.png"/>
-    //         <img class="post" src="./img/post2.png"/>
-    //         <img class="post" src="./img/post3.png"/>
-    //         <img class="post" src="./img/post4.png"/>
-    //         <img class="post" src="./img/post5.png"/>
-    //         <img class="post" src="./img/post6.png"/>
-    //         <img class="post" src="./img/post1.png"/>
-    //         <img class="post" src="./img/post2.png"/>
-    //         <img class="post" src="./img/post3.png"/>
-    //         <img class="post" src="./img/post4.png"/>
-    //         <img class="post" src="./img/post5.png"/>
-    //         <img class="post" src="./img/post6.png"/>
-    //         <img class="post" src="./img/post1.png"/>
-    //         <img class="post" src="./img/post2.png"/>
-    //         <img class="post" src="./img/post3.png"/>
-    //         <img class="post" src="./img/post4.png"/>
-    //         <img class="post" src="./img/post5.png"/>
-    //         <img class="post" src="./img/post6.png"/>
-    //         <img class="post" src="./img/post5.png"/>
-    //         <img class="post" src="./img/post6.png"/>
-    //       </div>
-    //       <img src="./img/add.png" id="add-button"/>
-    //   </div>
-    // </div>
   );
 }
 
